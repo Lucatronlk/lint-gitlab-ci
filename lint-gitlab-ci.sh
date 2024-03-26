b46bbef82c25e995ed3d510f7e890cfbb37fd328
luca <alex.luka@hotmail.com>
2024-03-26 17:04:48 +0200

    adapt it for gitlab

--- lint-gitlab-ci.sh
+++ lint-gitlab-ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
+GITLAB_TOKEN=""
 # GITLAB_SERVER_URL=${GITLAB_SERVER_URL:-"gitlab.com"}
 # ## get gitlab push remote
 GITLAB_REMOTE=$(git remote -v | grep push | awk '{print $2}')
@@ -13,53 +13,58 @@ GITLAB_REMOTE=${GITLAB_REMOTE#*//}
 GITLAB_REMOTE=${GITLAB_REMOTE%.git}
 
 # ## get the url
-GITLAB_SERVER_URL=$(echo "${GITLAB_REMOTE:?}" | cut -d':' -f1)
-# ## get the project name and URL encode
-GITLAB_PROJECT=$(echo "${GITLAB_REMOTE:?}" | cut -d':' -f2 | sed 's#/#%2F#g' )
-# ## get project ID
-# GITLAB_PROJECT_ID=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN:?}" "https://${GITLAB_SERVER_URL:?}/api/v4/projects/${GITLAB_PROJECT:?}" | jq '.id')
+#GITLAB_SERVER_URL=$(echo "${GITLAB_REMOTE:?}" | cut -d':' -f1)
+GITLAB_SERVER_URL=$(echo "${GITLAB_REMOTE:?}" | cut -d'/' -f1)
 
-CI_FILE=${1:-".gitlab-ci.yml"}
-TOKEN=${GITLAB_TOKEN:-""}
-# ## ENDPOINT: see https://docs.gitlab.com/ee/api/lint.html
-# ## old endpoint, deprecated since 15.7, removed in 16.0
-# API_ENDPOINT=ci/lint
-# ## new endpoint: /projects/:id/ci/lint
-API_ENDPOINT=projects/${GITLAB_PROJECT:?}/ci/lint
-URL="https://${GITLAB_SERVER_URL}/api/v4/${API_ENDPOINT:?}?include_merged_yaml=true"
+echo $GITLAB_SERVER_URL
+# # ## get the project name and URL encode
+# GITLAB_PROJECT=$(echo "${GITLAB_REMOTE:?}" | cut -d':' -f2 | sed 's#/#%2F#g' )
+GITLAB_PROJECT=$(echo "${GITLAB_REMOTE:?}" | cut -d'/' -f5 )
+echo $GITLAB_PROJECT
+# # ## get project ID
+GITLAB_PROJECT_ID=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN:?}" "https://${GITLAB_SERVER_URL:?}/api/v4/projects/${GITLAB_PROJECT:?}" | jq '.id')
 
-stderr() {
-  printf >&2 "Error: %s\n" "$@"
-}
+# CI_FILE=${1:-".gitlab-ci.yml"}
+# TOKEN=${GITLAB_TOKEN:-""}
+# # ## ENDPOINT: see https://docs.gitlab.com/ee/api/lint.html
+# # ## old endpoint, deprecated since 15.7, removed in 16.0
+# # API_ENDPOINT=ci/lint
+# # ## new endpoint: /projects/:id/ci/lint
+# API_ENDPOINT=projects/${GITLAB_PROJECT:?}/ci/lint
+# URL="https://${GITLAB_SERVER_URL}/api/v4/${API_ENDPOINT:?}?include_merged_yaml=true"
 
-validate() {
-  response=$(jq --null-input --arg yaml "$(< "$CI_FILE")" '.content=$yaml' | curl -s -H "PRIVATE-TOKEN: ${TOKEN}" -H 'Content-Type: application/json' "$URL" --data @-)
+# stderr() {
+#   printf >&2 "Error: %s\n" "$@"
+# }
 
-  valid=$(echo "$response" | jq '.valid')
-  warnings=$(echo "$response" | jq '.warning')
-  errors=$(echo "$response" | jq '.errors')
+# validate() {
+#   response=$(jq --null-input --arg yaml "$(< "$CI_FILE")" '.content=$yaml' | curl -s -H "PRIVATE-TOKEN: ${TOKEN}" -H 'Content-Type: application/json' "$URL" --data @-)
 
-  [[ "$valid" == "true" ]] && {
-    printf "%s validated.\n" "$CI_FILE"
-    exit 0
-  }
-  [[ "$warnings" != "null" ]] && {
-    printf "Warnings: %s\n" "$warnings"
-    exit 1
-  }
-  [[ "$errors" != "null" ]] && {
-    stderr "$errors"
-    exit 1
-  }
-}
+#   valid=$(echo "$response" | jq '.valid')
+#   warnings=$(echo "$response" | jq '.warning')
+#   errors=$(echo "$response" | jq '.errors')
 
-main() {
-  [[ "$TOKEN" == "" ]] && stderr "GITLAB_TOKEN needs to be set."
-  [[ $(command -v "jq") ]] || stderr "jq needs to be installed."
-  [[ $(command -v "curl") ]] || stderr "curl needs to be installed."
-  [[ -f "$CI_FILE" ]] || stderr "$CI_FILE does not exist."
+#   [[ "$valid" == "true" ]] && {
+#     printf "%s validated.\n" "$CI_FILE"
+#     exit 0
+#   }
+#   [[ "$warnings" != "null" ]] && {
+#     printf "Warnings: %s\n" "$warnings"
+#     exit 1
+#   }
+#   [[ "$errors" != "null" ]] && {
+#     stderr "$errors"
+#     exit 1
+#   }
+# }
 
-  validate
-}
+# main() {
+#   [[ "$TOKEN" == "" ]] && stderr "GITLAB_TOKEN needs to be set."
+#   [[ $(command -v "jq") ]] || stderr "jq needs to be installed."
+#   [[ $(command -v "curl") ]] || stderr "curl needs to be installed."
+#   [[ -f "$CI_FILE" ]] || stderr "$CI_FILE does not exist."
 
-[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
+#   validate
+# }
+
+# [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
