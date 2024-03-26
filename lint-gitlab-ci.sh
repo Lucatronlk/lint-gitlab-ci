489eeba14d9bf0cc7b37db1fe032f0759ce69b89
luca <alex.luka@hotmail.com>
2024-03-26 17:29:02 +0200

    fix errors

--- lint-gitlab-ci.sh
+++ lint-gitlab-ci.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-GITLAB_TOKEN=""
 # GITLAB_SERVER_URL=${GITLAB_SERVER_URL:-"gitlab.com"}
 # ## get gitlab push remote
 GITLAB_REMOTE=$(git remote -v | grep push | awk '{print $2}')
@@ -22,49 +21,51 @@ echo $GITLAB_SERVER_URL
 GITLAB_PROJECT=$(echo "${GITLAB_REMOTE:?}" | cut -d'/' -f5 )
 echo $GITLAB_PROJECT
 # # ## get project ID
-GITLAB_PROJECT_ID=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN:?}" "https://${GITLAB_SERVER_URL:?}/api/v4/projects/${GITLAB_PROJECT:?}" | jq '.id')
+GITLAB_PROJECT_ID=$(curl -s --header "PRIVATE-TOKEN: ${GITLAB_TOKEN:?}" "https://${GITLAB_SERVER_URL:?}/api/v4/search?scope=projects&search=${GITLAB_PROJECT:?}" | jq '.[0].id')
+echo $GITLAB_PROJECT_ID
+CI_FILE=${1:-".gitlab-ci.yml"}
+TOKEN=${GITLAB_TOKEN:-""}
+# ## ENDPOINT: see https://docs.gitlab.com/ee/api/lint.html
+# ## old endpoint, deprecated since 15.7, removed in 16.0
+# API_ENDPOINT=ci/lint
+# ## new endpoint: /projects/:id/ci/lint
+API_ENDPOINT=projects/${GITLAB_PROJECT_ID:?}/ci/lint
+echo $API_ENDPOINT
+URL="https://${GITLAB_SERVER_URL}/api/v4/${API_ENDPOINT:?}?include_merged_yaml=true"
+echo $URL
 
-# CI_FILE=${1:-".gitlab-ci.yml"}
-# TOKEN=${GITLAB_TOKEN:-""}
-# # ## ENDPOINT: see https://docs.gitlab.com/ee/api/lint.html
-# # ## old endpoint, deprecated since 15.7, removed in 16.0
-# # API_ENDPOINT=ci/lint
-# # ## new endpoint: /projects/:id/ci/lint
-# API_ENDPOINT=projects/${GITLAB_PROJECT:?}/ci/lint
-# URL="https://${GITLAB_SERVER_URL}/api/v4/${API_ENDPOINT:?}?include_merged_yaml=true"
+stderr() {
+  printf >&2 "Error: %s\n" "$@"
+}
 
-# stderr() {
-#   printf >&2 "Error: %s\n" "$@"
-# }
+validate() {
+  response=$(jq --null-input --arg yaml "$(< "$CI_FILE")" '.content=$yaml' | curl -s -H "PRIVATE-TOKEN: ${TOKEN}" -H 'Content-Type: application/json' "$URL" --data @-)
 
-# validate() {
-#   response=$(jq --null-input --arg yaml "$(< "$CI_FILE")" '.content=$yaml' | curl -s -H "PRIVATE-TOKEN: ${TOKEN}" -H 'Content-Type: application/json' "$URL" --data @-)
+  valid=$(echo "$response" | jq '.valid')
+  warnings=$(echo "$response" | jq '.warning')
+  errors=$(echo "$response" | jq '.errors')
 
-#   valid=$(echo "$response" | jq '.valid')
-#   warnings=$(echo "$response" | jq '.warning')
-#   errors=$(echo "$response" | jq '.errors')
+  [[ "$valid" == "true" ]] && {
+    printf "%s validated.\n" "$CI_FILE"
+    exit 0
+  }
+  [[ "$warnings" != "null" ]] && {
+    printf "Warnings: %s\n" "$warnings"
+    exit 1
+  }
+  [[ "$errors" != "null" ]] && {
+    stderr "$errors"
+    exit 1
+  }
+}
 
-#   [[ "$valid" == "true" ]] && {
-#     printf "%s validated.\n" "$CI_FILE"
-#     exit 0
-#   }
-#   [[ "$warnings" != "null" ]] && {
-#     printf "Warnings: %s\n" "$warnings"
-#     exit 1
-#   }
-#   [[ "$errors" != "null" ]] && {
-#     stderr "$errors"
-#     exit 1
-#   }
-# }
+main() {
+  [[ "$TOKEN" == "" ]] && stderr "GITLAB_TOKEN needs to be set."
+  [[ $(command -v "jq") ]] || stderr "jq needs to be installed."
+  [[ $(command -v "curl") ]] || stderr "curl needs to be installed."
+  [[ -f "$CI_FILE" ]] || stderr "$CI_FILE does not exist."
 
-# main() {
-#   [[ "$TOKEN" == "" ]] && stderr "GITLAB_TOKEN needs to be set."
-#   [[ $(command -v "jq") ]] || stderr "jq needs to be installed."
-#   [[ $(command -v "curl") ]] || stderr "curl needs to be installed."
-#   [[ -f "$CI_FILE" ]] || stderr "$CI_FILE does not exist."
+  validate
+}
 
-#   validate
-# }
-
-# [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
+[[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
